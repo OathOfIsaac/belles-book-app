27a7c2d11053dee31e50486f9319024ffbc1334f
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-05 15:39:06 -0500

    upgraded react to 17.0.2, implemented App.js to use Routes insteaad of switch, refractored server.js to use Apollo (for real this time)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -37,11 +37,22 @@ function App() {
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
+          <div className='container'>
+          <Routes>
+          <Route 
+              path='/' 
+              element={<SearchBooks />} 
+            />
+            <Route 
+              path='/saved' 
+              element={<SavedBooks />} 
+            />
+            <Route 
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+          </div>
         </div>
       </Router>
     </ApolloProvider>
