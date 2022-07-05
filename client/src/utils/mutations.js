e36b0d3cf507b20ff99b98c13d3fd0610d708fd4
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-05 00:00:36 -0500

    refractored SavedBooks.js to work with Apollo, removed useEffect hook and replaced with useQuery, added utils/queries.js with GET_ME query, removed restful api deletebook function and refractored to useMutation. Added removeBook to utils/mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,3 +17,21 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }  
+    }
+  }
+`;
