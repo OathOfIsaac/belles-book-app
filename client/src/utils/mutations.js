755d0b756ddd4c09d4d9985f487ad138ed5560ce
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-04 22:46:53 -0500

    refractored RESTful api in Searchbooks.js to use useMutation SAVE_BOOK mutation. Added utils/mutations.js to hold the mutation.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
