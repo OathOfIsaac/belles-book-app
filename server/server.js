54c97c7e7955534a3c3fcff1f51ec73dd95628b4
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-06 01:45:24 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
+//praying that this code works its 1:44 am
 const { typeDefs, resolvers } = require('./Schemas');
 const db = require('./config/connection');
 
