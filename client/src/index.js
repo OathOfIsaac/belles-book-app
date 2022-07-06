45b41e1b2efa89773e9ecef07ac59c9617ad43bd
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-06 02:18:33 -0500

    new config method

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-
+//2:18 am
 
 ReactDOM.render(
   <React.StrictMode>
