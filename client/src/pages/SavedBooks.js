0dc9359428341a2f9896532169de829f5209704e
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-06 20:43:15 -0500

    bugfix SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,4 @@
-//import React, { useState, useEffect } from "react";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Jumbotron, Container, CardColumns, Card, Button } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { getMe, deleteBook } from "../utils/API";
@@ -9,38 +8,39 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
   
   const { loading, data } = useQuery(GET_ME);
 
-  setUserData(data?.me || []) 
+  const userData = data?.me || [];
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-          return false;
-        }
+  //       if (token) {
+  //         return;
+  //       }
 
-        const response = await getMe(token);
+  //       const response = await getMe(token);
 
-        if (!response.ok) {
-          throw new Error("something went wrong!");
-        }
+  //       if (!response.ok) {
+  //         throw new Error("something went wrong!");
+  //       }
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  //       const user = await response.json();
+  //       // setUserData(user);
+  //       console.log(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
 
-    getUserData();
-  }, []);
+  //   // getUserData();
+  // }, [userDataLength]);
 
   const [ deleteBook, { error } ] = useMutation(REMOVE_BOOK);
 
